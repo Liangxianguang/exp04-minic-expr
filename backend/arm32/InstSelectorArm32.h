1c0fc79d6a44b84f705ea39dcabc17ba5880c387
Liangxianguang <2811306715@qq.com>
2025-05-08 18:27:59 +0800

    Update project files

--- backend/arm32/InstSelectorArm32.h
+++ backend/arm32/InstSelectorArm32.h
@@ -93,9 +93,75 @@ protected:
 
 	/// @brief 整数求余指令翻译成ARM32汇编
 	/// @param inst IR指令
+/// @brief 整数求余指令翻译成ARM32汇编
+/// @param inst IR指令
 	void translate_mod_int32(Instruction * inst)
 	{
-		translate_two_operator(inst, "srem");
+		Value * result = inst;
+		Value * arg1 = inst->getOperand(0);
+		Value * arg2 = inst->getOperand(1);
+
+		int32_t arg1_reg_no = arg1->getRegId();
+		int32_t arg2_reg_no = arg2->getRegId();
+		int32_t result_reg_no = inst->getRegId();
+		int32_t load_result_reg_no, load_arg1_reg_no, load_arg2_reg_no;
+
+		// 看arg1是否是寄存器，若是则寄存器寻址，否则要load变量到寄存器中
+		if (arg1_reg_no == -1) {
+			// 分配一个寄存器r8
+			load_arg1_reg_no = simpleRegisterAllocator.Allocate(arg1);
+			// arg1 -> r8
+			iloc.load_var(load_arg1_reg_no, arg1);
+		} else {
+			load_arg1_reg_no = arg1_reg_no;
+		}
+
+		// 看arg2是否是寄存器，若是则寄存器寻址，否则要load变量到寄存器中
+		if (arg2_reg_no == -1) {
+			// 分配一个寄存器r9
+			load_arg2_reg_no = simpleRegisterAllocator.Allocate(arg2);
+			// arg2 -> r9
+			iloc.load_var(load_arg2_reg_no, arg2);
+		} else {
+			load_arg2_reg_no = arg2_reg_no;
+		}
+
+		// 看结果变量是否是寄存器，若不是则需要分配一个新的寄存器来保存运算的结果
+		if (result_reg_no == -1) {
+			// 分配一个寄存器r10，用于暂存结果
+			load_result_reg_no = simpleRegisterAllocator.Allocate(result);
+		} else {
+			load_result_reg_no = result_reg_no;
+		}
+
+		// 计算商
+		iloc.inst("sdiv", 
+				PlatformArm32::regName[load_result_reg_no],
+				PlatformArm32::regName[load_arg1_reg_no],
+				PlatformArm32::regName[load_arg2_reg_no]);
+
+		// 计算商与除数的乘积
+		iloc.inst("mul",
+				PlatformArm32::regName[load_result_reg_no],
+				PlatformArm32::regName[load_result_reg_no],
+				PlatformArm32::regName[load_arg2_reg_no]);
+
+		// 计算余数：被除数 - (商 * 除数)
+		iloc.inst("sub",
+				PlatformArm32::regName[load_result_reg_no],
+				PlatformArm32::regName[load_arg1_reg_no],
+				PlatformArm32::regName[load_result_reg_no]);
+
+		// 结果不是寄存器，则需要把结果保存到结果变量中
+		if (result_reg_no == -1) {
+			// r10 -> result
+			iloc.store_var(load_result_reg_no, result, ARM32_TMP_REG_NO);
+		}
+
+		// 释放寄存器
+		simpleRegisterAllocator.free(arg1);
+		simpleRegisterAllocator.free(arg2);
+		simpleRegisterAllocator.free(result);
 	}
 
     /// @brief 二元操作指令翻译成ARM32汇编
